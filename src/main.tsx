c199d4b5630ac96b0dfd26c9f32ffa5ea717d6e2
Shubham Maurya <skmaurya9721@gmail.com>
2025-02-17 22:21:19 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { StrictMode } from "react";
+import { routeTree } from "./routeTree.gen";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const queryClient = new QueryClient();
+const container = document.getElementById("root") as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
